52a1d8f1e195851ed8f114aa17831d82b939d675
rohanrakeshjain26 <rohanrakeshjain26@gmail.com>
2021-07-12 00:07:44 +0530

    Added sanitization for input and improved error handling in Geektrust.

--- src/main/Geektrust.java
+++ src/main/Geektrust.java
@@ -9,13 +9,8 @@ import java.util.*;
 
 
 public class Geektrust {
-    static UserLoanService userLoanService;
-    static PaymentService paymentService;
 
     public static void main(String[] args) {
-        userLoanService = new UserLoanService();
-        paymentService = new PaymentService();
-
         try {
             String filePath = args[0];
             FileInputStream fis = new FileInputStream(filePath);
@@ -26,23 +21,26 @@ public class Geektrust {
                 String str[] = s.split(" ");
                 String bankName = str[1];
                 String userName = str[2];
+                str[0] = str[0].toUpperCase();
 
                 if (str[0].equals("LOAN")) {
                     int prAmount = Integer.parseInt(str[3]);
                     int noOfYear = Integer.parseInt(str[4]);
                     int interestRate = Integer.parseInt(str[5]);
-                    userLoanService.addLoan(bankName, userName, prAmount, noOfYear, interestRate);
+                    UserLoanService.addLoan(bankName, userName, prAmount, noOfYear, interestRate);
                 } else if (str[0].equals("BALANCE")) {
                     int emiNo = Integer.parseInt(str[3]);
-                    userLoanService.printAmountPaidAndEmiLeft(bankName,userName,emiNo);
-                } else {
+                    UserLoanService.printAmountPaidAndEmiLeft(bankName,userName,emiNo);
+                } else if(str[0].equals("PAYMENT")) {
                     int lumpSumAmount = Integer.parseInt(str[3]);
                     int emiNo = Integer.parseInt(str[4]);
-                    paymentService.addPayment(bankName,userName,lumpSumAmount,emiNo);
+                    PaymentService.addPayment(bankName,userName,lumpSumAmount,emiNo);
+                } else{
+                    throw new Exception("Invalid input!!");
                 }
             }
             sc.close();
-        } catch (IOException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
